50315289ed25b877b088dfc4847b172a60c8a9b1
Algirdas Tamasauskas <algirdastamasauskas@MacBook-Pro-2.local>
2021-03-10 19:23:14 +0200

    Remove offline support + dynamic pages

--- src/templates/landing.jsx
+++ src/templates/landing.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import { graphql } from "gatsby";
 import Layout from "../layout";
 import Block from "../components/Block/Block";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
 
-function Landing({ data }) {
-  const postEdges = data.allMarkdownRemark.edges;
+function Landing() {
   return (
     <Layout>
       <Helmet title={config.siteTitle} />
@@ -18,27 +16,3 @@ function Landing({ data }) {
 }
 
 export default Landing;
-
-/* eslint no-undef: "off" */
-export const pageQuery = graphql`
-  query LandingQuery {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
-      }
-    }
-  }
-`;
